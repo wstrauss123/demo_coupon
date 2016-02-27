ada2107e958f3ae0016173a784646627c0e53f47
wstrauss123 <wstrauss123@gmail.com>
2016-02-27 13:28:45 -0800

    modified thank you

--- js/thank_you.js
+++ js/thank_you.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 	// parse out first name from cookie and write to DOM
-	var pattern = /firstName=(.*)_lastName/i;
+	var pattern = /firstName=(.*)\;/i;
 	if (pattern.exec(document.cookie)[1] === '') {
-		//$("#firstName").html("Avid shopper");
 		return;
 	} else {
-    	$("#firstName").html(pattern.exec(document.cookie)[1]);
+    	$("#name").html(pattern.exec(document.cookie)[1]);
     }
 });
+
+
+
+
